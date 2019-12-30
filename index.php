ee005913f06d965b71df9868e31b4e22027587f6
Tchouamou Eric herve <tchouamou@gmail.com>
2019-12-30 02:17:36 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * index.php
+ *         --------
+ * begin    : July 2004 - October 2011
+ * copyright   : Camaroes Ver 3.03 (C) 2004-2011 T.E.H
+ * www     : http://sourceforge.net/projects/camaroes/
+ */
+
+/*  @license http://www.gnu.org/copyleft/gpl.html GNU/GPL */
+/*
+Copyright (c) 2011, Tchouamou Eric Herve  <tchouamou@gmail.com>
+All rights reserved.
+
+
+
+
+
+
+
+
+
+index.php,  2011-Oct
+*/
+
+
+/*=====================*/
+define("cmr_online", "1");//-- Control when loading module --
+/*=====================*/
+// 
+// ( \/ ) / _\ (  )(  ( \
+// / \/ \/    \ )( /    /
+// \_)(_/\_/\_/(__)\_)__)
+// 
+//  Main Camaroes
+// 
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+include_once(dirname(__FILE__) . "/camaroes_class.php");
+include_once(dirname(__FILE__) . "/common_begin.php");
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+// $cmr->debug_print();exit;
+
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+include(dirname(__FILE__) . "/front_page.php");
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+include_once(dirname(__FILE__) . "/common_end.php");
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+?>
